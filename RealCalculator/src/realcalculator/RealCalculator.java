85ef865801c9500dcb1911dde08de3a34be7cd7d
OumarDiop <oumardiop2003@gmail.com>
2019-05-30 16:20:03 -0700

    First Version of the Calculator from Netbeans
    First Version of the Calculator from Netbeans

---
+++ RealCalculator/src/realcalculator/RealCalculator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package realcalculator;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author coolDawg1234
+ */
+public class RealCalculator {
+
+    
+    
+    
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        RealCalculator realCalc = new RealCalculator();
+        JFrame CalcFrame = new CalcFrame();
+        CalcFrame.setVisible(true);
+    }
+    
+}
